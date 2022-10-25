e2ebb916efd7237647d7f56d4fd46a85c3e1d343
Kirkendolled <112414443+Kirkendolled@users.noreply.github.com>
2022-10-25 17:37:39 -0500

    Final project

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let detailsForm=document.querySelector('#destination_details_form"'); // using # sign here because of querySelector
+(function(){
+"use strict";
+
+
+const detailsForm = document.querySelector('#destination_details_form'); // using # sign here because of querySelector
 detailsForm.addEventListener('submit', handleFormSubmit); //handleFormSubmit is a NAMED FUNCTION
 
-//I put a funtion diagram in with the saved notes to show each step to take and how to set up a flow chart
+//I put a funtion diagram in with the saved notes to show each step to take 
+//and how to set up a flow chart
 
 function handleFormSubmit (event){
   event.preventDefault();
@@ -11,13 +16,67 @@ function handleFormSubmit (event){
    let destDesc=event.target.elements["description"].value;
 
    //this loop clears the form when it's submitted
-   for (var i=0; i < detailsForm.length; i++) {
+   for (let i=0; i < detailsForm.length; i++) {
     detailsForm.elements[i].value="";
    }
 
-   //create card here...
+   let destCard=createDestinationcard(destName, destLocation, destPhoto, destDesc);
 
-   var wishListContainer=document.getElementById('destinations_container');
-   if (wishListContainer.children.length === 0) //if there is nothing in the container then 
+   let wishListContainer=document.getElementById('destinations_container');
+
+   if (wishListContainer.children.length === 0) { //if there is nothing in the container then 
    document.getElementById('title').innerHTML = "My Wish List";
+   }
+   document.querySelector("#destinations_container").appendChild(destCard);
 }
+
+
+function createDestinationcard(name,location, photoURL, description){
+  let card = document.createElement("div");
+  card.className = 'card';
+  let img=document.createElement('img');
+  img.setAttribute('alt', name);
+
+  let constantPhotoURL="images/signpost.jpg";
+
+  if(photoURL.length === 0) {
+    img.setAttribute('src', constantPhotoURL);
+  }
+  else {
+    img.setAttribute('src', photoURL)
+  }
+
+  card.appendChild(img);
+
+  const cardBody=document.createElement('div')
+  cardBody.className = "card-body";
+
+  const cardTitle=document.createElement('h3');
+  cardTitle.innerHTML=name;
+  cardBody.appendChild(cardTitle);
+
+  const cardSubtitle=document.createElement('h4');
+  cardSubtitle.innerText= location;
+  cardBody.appendChild(cardSubtitle);
+
+  if(description.length !== 0) {
+    let cardText=document.createElement('p');
+    cardText.className="card-text";
+    cardText.innerText=description;
+    cardBody.appendChild(cardText);
+  }
+
+  let cardDeleteBtn=document.createElement('button');
+  cardDeleteBtn.innerText="Remove"
+
+  cardDeleteBtn.addEventListener('click', removeDestination);
+  cardBody.appendChild(cardDeleteBtn);
+  card.appendChild(cardBody);
+  return card;
+}
+
+function removeDestination(event){
+  let card=event.target.parentElement.parentElement;
+  card.remove();
+}
+})();
